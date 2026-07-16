514bb1d63d063e4910d24a1fc43f053c9f2728df
rjvanaken <rjvanaken@gmail.com>
2026-07-16 18:23:21 -0400

    Hype boost card visuals , and added fields

--- src/components/custom/HypePostEntry.tsx
+++ src/components/custom/HypePostEntry.tsx
@@ -18,6 +18,7 @@ function HypePostEntry({ onClose}: HypePostEntryProps) {
 
   const navigate = useNavigate()
   const [task, setTask] = useState('')
+  const [image, uploadImage] = useState('')
 
   return (
     <TopPage>
@@ -33,10 +34,10 @@ function HypePostEntry({ onClose}: HypePostEntryProps) {
         <X className="size-5" aria-hidden="true" />
             </Button>
       </CardHeader>
-      <CardContent className='mt-2'>
+      <CardContent className='mt-2 flex flex-col gap-5'>
         <FormSelectField
           onValueChange={(value) => setTask(value ?? '')}
-          label="What did you get done?"
+          label="Task"
           id="task"
           placeholder="Pick a task..."
           options={[
@@ -46,10 +47,24 @@ function HypePostEntry({ onClose}: HypePostEntryProps) {
           ]}
         />
         <FormField
+        className='border=0.25'
           id="note"
           multiline
           label={<>Note <span className="text-primary">(optional)</span></>}
-        />
+          placeholder="How did it feel?"
+          />
+
+          <FormField
+          className='border-0.25'
+          id="picture"
+          file
+          label={<>Note <span className="text-primary">(optional)</span></>}
+          value={image}
+          onChange={(e) => uploadImage(e.target.value)}
+          />
+
+
+
       </CardContent>
       <CardFooter className="flex flex-col gap-3 mt-2">
         <Button size="lg" className="w-full" disabled={!task} onClick={() => navigate('/home')}>  {/* temporary */}
