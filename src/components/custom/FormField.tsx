41279358be9b5a95b7fc1cdd2416e09ebacd2c7d
rjvanaken <rjvanaken@gmail.com>
2026-07-16 17:55:20 -0400

    adjusted formfield to accept image upload

--- src/components/custom/FormField.tsx
+++ src/components/custom/FormField.tsx
@@ -6,15 +6,24 @@ interface FormFieldProps extends React.ComponentProps<'input'> {
   label: React.ReactNode
   id: string
   multiline?: boolean
+  file?: boolean
 }
 
-function FormField({ label, id, className, multiline = false, ...props }: FormFieldProps) {
+function FormField({ label, id, className, multiline = false, file = false, ...props }: FormFieldProps) {
   return (
     <div className="flex flex-col gap-2">
       <Label htmlFor={id} className="text-left text-sm font-medium text-secondary placeholder:text-text-neutral-500">
         {label}
       </Label>
-      {multiline ? (
+      {file ? (
+        <Input
+          id={id}
+          type="file"
+          accept="image/*"
+          className={className ?? 'mb-2 w-full border-0.25 border-neutral-300 px-2.5 py-2 h-11'}
+          {...props}
+        />
+      ) : multiline ? (
         <Textarea
           id={id}
           className={className ?? 'mb-2 w-full border-0.25 border-neutral-300 px-2.5 py-2'}
